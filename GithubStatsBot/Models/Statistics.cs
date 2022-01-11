f1373c14cea1d38702e7b89186334513a241f7e7
osadchik <osadchiik@gmail.com>
2022-01-11 19:21:01 +0200

    Create statistics entity

---
+++ GithubStatsBot/Models/Statistics.cs
@@ -0,0 +1,15 @@
+﻿using Octokit;
+
+namespace GithubStatsBot.Models
+{
+    public class Statistics
+    {
+        public TimeSpan AvgTimeBeforeFirstAnswer { get; set; }
+        public TimeSpan AvgTimeBeforeBeingClosed { get; set; }
+        public ushort NumberOfIssuesCreated { get; set; }
+        public ushort NumberOfIssuesClosed { get; set; }
+        public Issue MostMentionedTopic { get; set; }
+        public IList<Issue> UnansweredIssues { get; set; }
+
+    }
+}
