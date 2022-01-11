8f3ce38920b3664b5829b9961c168bb03babec18
osadchik <osadchiik@gmail.com>
2022-01-11 22:34:51 +0200

    Create StatisticsService with basic functionality

---
+++ GithubStatsBot/Services/StatisticsService.cs
@@ -0,0 +1,68 @@
+﻿using GithubStatsBot.Models;
+using System.Linq;
+using Octokit;
+
+namespace GithubStatsBot.Services
+{
+    public class StatisticsService
+    {
+        private GitHubClient client;
+
+        public StatisticsService(GitHubClient client)
+        {
+            this.client = client;
+        }
+
+        public Statistics CollectStatistics()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async TimeSpan GetAverageTimeBeforeFirstAnswer()
+        {
+            List<TimeSpan> total = new List<TimeSpan>();
+            var issues = await client.Issue.GetAllForRepository("noobquire", "github-stats-bot");
+
+            foreach (var issue in issues)
+            {
+                var comments = await client.Issue.Comment.GetAllForIssue("noobquire", "github-stats-bot", issue.Number);
+                var firstComment = comments.FirstOrDefault(c => c.User.Permissions.Admin);
+
+                if (firstComment is null)
+                {
+                    continue;
+                }
+
+                total.Add(firstComment.CreatedAt - issue.CreatedAt);
+            }
+
+            return TimeSpan.FromHours(total.Select(s => s.TotalHours).Average());
+        }
+
+        private async TimeSpan GetAverageTimeBeforeBeingClosed()
+        {
+            List<TimeSpan> total = new List<TimeSpan>();
+            var issues = await client.Issue.GetAllForRepository("noobquire", "github-stats-bot");
+
+            foreach (var issue in issues)
+            {
+
+            }
+        }
+
+        private ushort GetNumberOfIssuesCreated()
+        {
+            throw new NotImplementedException();
+        }
+
+        private ushort GetNumberOfIssuesClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Issue> GetAnsweredIssues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
