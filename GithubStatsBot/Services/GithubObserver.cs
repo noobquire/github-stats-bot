a4c179a4bf9f02c8fa93a204248dad1ce2ad5997
Oleksii Lytvynov <ostrich.alexey@gmail.com>
2022-01-11 19:12:06 +0200

    added octokit lib and service template

---
+++ GithubStatsBot/Services/GithubObserver.cs
@@ -0,0 +1,31 @@
+﻿using Octokit.Reactive;
+using Octokit;
+using System.Linq;
+
+namespace GithubStatsBot.Services
+{
+    public class GithubObserver : IObserver<Repository>
+    {
+        private readonly IObservableGitHubClient _gitHubClient;
+
+        public GithubObserver()
+        {
+            _gitHubClient = new ObservableGitHubClient(new ProductHeaderValue("noobquire"));
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Repository value)
+        {
+            value.
+        }
+    }
+}
